1e24a627d191586f82599918db8a6541d1e2cb9d
Israel Rotimi <izzyrotimi@gmail.com>
2025-03-20 08:19:11 +0100

    feat: add SpecialOffer section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ text, iconUrl }) => {
+const Button = ({ text, iconUrl, primary=true }) => {
   return (
-    <button className="flex gap-2 rounded-full px-5 py-3 bg-coral-red text-white w-36">
+    <button className={`flex justify-center gap-2 rounded-full px-5 py-3 w-40 ${primary ? 'bg-coral-red text-white' : 'info-text border border-gray-500'}`}>
         {text}
         {iconUrl && 
         (<img src={iconUrl} alt="" />)}
--- src/sections/SpecialOffer.jsx
+++ src/sections/SpecialOffer.jsx
@@ -1,6 +1,34 @@
+import {offer} from "../assets/images"
+import {arrowRight} from "../assets/icons"
+import Button from "../components/Button"
+
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <div 
+    className="flex flex-col md:flex-row gap-10">
+      <div className="flex max-md:order-1 md:w-[120%] md:h-[120%]">
+        <img src={offer} alt=""
+        className="w-full h-full" />
+      </div>
+      <div className="flex flex-col gap-5 my-10">
+        <h2 
+        className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Special </span>
+          Offer
+        </h2>
+        <p className="info-text font-montserrat">
+        Embark on a shopping journey that redefines your experience with unbeatable deals. From premier selections to incredible savings, we offer unparalleled value that sets us apart.
+        </p>
+        <p 
+        className="info-text font-montserrat">
+        Navigate a realm of possibilities designed to fulfill your unique desires, surpassing the loftiest expectations. Your journey with us is nothing short of exceptional.
+        </p>
+        <div className="flex gap-10">
+        <Button text="Shop now" iconUrl={arrowRight} />
+        <Button text="Learn more" primary={false} />
+        </div>
+      </div>
+    </div>
   )
 }
 
