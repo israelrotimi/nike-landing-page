ee586ec78136a4859cab3520fcba4cc691026dd0
Israel Rotimi <izzyrotimi@gmail.com>
2025-03-20 09:46:57 +0100

    feat: add Services

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,20 @@
+import { services } from "../constants"
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <div 
+    className="w-full my-4">
+      <div className="flex flex-wrap gap-10">
+        {services.map(({img, label, text}, i) => (
+          <div key={i}
+          className="flex flex-col gap-5 w-80 shadow p-6">
+          <img src={img} alt={label} />
+          <h3 className="text-2xl">{label}</h3>
+          <p className="info-text font-montserrat">{text}</p>
+        </div>
+        ))}
+      </div>
+    </div>
   )
 }
 
